d2f8a4569513fa2b8b688ac73f8b737de5149ac8
Remi Lesieur <r.lesieur@goood.com>
2021-07-19 19:55:20 +0200

    Added global actions.

--- scripts/module.js
+++ scripts/module.js
@@ -77,14 +77,10 @@ async function _quickRollAttack(event) {
 }
 
 async function _sendRoll(actor, attackEntity, currentLevel = 0) {
-    if (attackEntity.hasDamage) {
-        console.log("Has Damage")
-    }
+    if (!attackEntity.hasDamage) {
 
-    var d = attackEntity.getAttackToHit()
+        var components = (attackEntity.labels.components || [])
 
-
-    if (d == null) {
         var spellDesc = await renderTemplate(folder + '/templates/spell-damage-only.hbs', {
             title: attackEntity.data.name,
             actor: actor.id,
@@ -93,7 +89,53 @@ async function _sendRoll(actor, attackEntity, currentLevel = 0) {
             description: attackEntity.data.data.description.value,
             spellLevel: attackEntity.labels.level,
             save: attackEntity.labels.save,
-            components: attackEntity.labels.components.join(',')
+            hasComponents : components.length > 0,
+            components: components.join(',')
+        })
+
+        await ChatMessage.create({
+            speaker: {actor: actor._id},
+            content: spellDesc
+        })
+
+
+        // console.log("No Damage")
+
+        // var components = (attackEntity.labels.components || [])
+
+        // var spellDesc = await renderTemplate(folder + '/templates/spell-damage-only.hbs', {
+        //     title: attackEntity.data.name,
+        //     actor: actor.id,
+        //     weapon: attackEntity.id,
+        //     img: attackEntity.data.img,
+        //     description: attackEntity.data.data.description.value,
+        //     spellLevel: attackEntity.labels.level,
+        //     save: attackEntity.labels.save,
+        //     hasComponents : components.length > 0,
+        //     components: components.join(',')
+        // })
+
+        // game.messages.postOne(spellDesc)
+
+        return
+    }
+
+    var d = attackEntity.getAttackToHit()
+
+
+    if (d == null) {
+        var components = (attackEntity.labels.components || [])
+
+        var spellDesc = await renderTemplate(folder + '/templates/spell-damage-only.hbs', {
+            title: attackEntity.data.name,
+            actor: actor.id,
+            weapon: attackEntity.id,
+            img: attackEntity.data.img,
+            description: attackEntity.data.data.description.value,
+            spellLevel: attackEntity.labels.level,
+            save: attackEntity.labels.save,
+            hasComponents : components.length > 0,
+            components: components.join(',')
         })
 
         attackEntity.rollDamage({
@@ -166,25 +208,27 @@ function rollAttack(event) {
     })
 }
 
-Hooks.once('init', async function () {
-    console.log('Initializing ...')
-});
+// Hooks.once('init', async function () {
+//     console.log('Initializing ...')
+// });
 
-Hooks.once('ready', async function () {
-    console.log('Readying ...')
-});
+// Hooks.once('ready', async function () {
+//     console.log('Readying ...')
+// });
 
 Hooks.on('renderActorSheet5e', async (app, html, data) => {
     player = data.actor._id
 
     var weapons = []
+    var actions = []
     var spellbook = []
-    
+
     data.spellbook.forEach(spellPage => {
-        var page = { 
-            level: spellPage.order, 
-            label : spellPage.label,
-            spells : spellPage.spells.filter(spell => spell.data.damage.parts.length > 0)}
+        var page = {
+            level: spellPage.order,
+            label: spellPage.label,
+            spells: spellPage.spells.filter(_isAttack)
+        }
         spellbook.push(page)
     })
 
@@ -195,20 +239,43 @@ Hooks.on('renderActorSheet5e', async (app, html, data) => {
     if (data.isCharacter) {
         data.inventory.forEach(itemList => {
             if (itemList.label === 'DND5E.ItemTypeWeaponPl') {
-                weapons = _getHasDamages(itemList)
-            }
-        });
-    } else if (data.isNPC) {
-        data.features.forEach(feat => {
-            if (feat.label === 'Attacks') {
-                weapons = _getHasDamages(feat)
+                weapons = _getAttacks(itemList)
             }
         });
     }
 
+    data.features.forEach(feat => {
+        if (feat.label === 'Attacks') {
+            weapons = weapons.concat(_getAttacks(feat))
+            return
+        }
+        if (feat.label === 'Actions') {
+            actions = actions.concat(_getAttacks(feat))
+        }
+        if (feat.label === 'DND5E.FeatureActive') {
+            actions = actions.concat(_getAttacks(feat))
+        }
+    });
+
+    actions = actions.reduce((arr, curr) => {
+        var key = curr.data.activation.type
+        if (arr[key]) {
+            arr[key].hasItems = true
+            arr[key].items.push(curr)
+        }
+        return arr
+    }, 
+    {
+        action: { hasItems : false, items : [] },
+        bonus: { hasItems : false, items : [] },
+        lair: { hasItems : false, items : [] },
+        legendary: { hasItems : false, items : [] },
+    })
+
     addActionsTab(app, html, {
         "weapons": weapons,
         "spellbook": spellbook,
+        "actions" : actions,
         "actorId": player,
         "disadvantage": data.advmode == "disadvantage",
         "normal": data.advmode == "normal",
@@ -223,9 +290,15 @@ Hooks.on('renderChatMessage', async (app, html, data) => {
 
     if (atk.length == 0)
         return
-    atk.on('click', { isCrit : hasCritical.length > 0 } ,rollAttack)
+    atk.on('click', {
+        isCrit: hasCritical.length > 0
+    }, rollAttack)
 })
 
-function _getHasDamages(itemList) {
-    return itemList.items.filter(i => i.data.damage.parts.length > 0)
+function _getAttacks(itemList) {
+    return itemList.items.filter(_isAttack)
+}
+
+function _isAttack(i) {
+    return i.data.damage.parts.length > 0 || i.data.actionType === 'save'
 }
